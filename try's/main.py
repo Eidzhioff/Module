ad87ddae695dfc59b0508ba3f474e6d96a912628
German <99397105+Eidzhioff@users.noreply.github.com>
2023-03-25 11:33:27 +0300

    Update main.py

--- try's/main.py
+++ try's/main.py
@@ -1,5 +1,5 @@
 import sortingMethods
 
-print(sortingMethods.insertion([1,4,-2,0,7,100]))
-print(sortingMethods.merge([1,4,-2,0,7,100]))
-print(sortingMethods.pyramid([1,4,-2,0,7,100]))
+print(sortingMethods.insertionSort([1,4,-2,0,7,100]))
+print(sortingMethods.mergeSort([1,4,-2,0,7,100]))
+print(sortingMethods.pyramidSort([1,4,-2,0,7,100]))
